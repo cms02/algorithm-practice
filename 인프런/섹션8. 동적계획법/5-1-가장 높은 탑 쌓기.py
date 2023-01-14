3ee0acbcb6d342e8babdf85a72c75de1dd69cef2
CHU MIN SEOK <66512686+cms02@users.noreply.github.com>
2023-01-14 16:54:18 +0000

    가장 높은 탑 쌓기

---
+++ 인프런/섹션8. 동적계획법/5-1-가장 높은 탑 쌓기.py
@@ -0,0 +1,25 @@
+n = int(input())
+bricks = []
+for i in range(n):
+    a, b, c = map(int, input().split())
+    bricks.append((a, b, c))
+bricks.sort(reverse=True)
+dy = [0] * n
+dy[0] = bricks[0][1]
+res = bricks[0][1]
+for i in range(1, n):
+    max_h = 0
+    for j in range(i - 1, -1, -1):
+        if bricks[j][2] > bricks[i][2] and dy[j] > max_h:
+            max_h = dy[j]
+    dy[i] = max_h + bricks[i][1]
+    res = max(res, dy[i])
+print(res)
+
+# 5
+# 25 3 4
+# 4 4 6
+# 9 2 3
+# 16 2 5
+# 1 5 2
+# 10
