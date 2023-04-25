abbbc1e1986cd20c3307f80d6239ae0458fe5a51
cms02 <minoui1234@gmail.com>
2023-04-25 22:34:12 +0000

    6-격자판 최대합

--- 인프런/excercise/3/6-격자판 최대합.py
+++ 인프런/excercise/3/6-격자판 최대합.py
@@ -1,5 +1,4 @@
 n = int(input())
-
 a = [list(map(int, input().split())) for _ in range(n)]
 
 largest = -2147000000
@@ -16,7 +15,7 @@ for i in range(n):
 sum1 = sum2 = 0
 for i in range(n):
     sum1 += a[i][i]
-    sum2 += a[i][n-i-1]
+    sum2 += a[i][n - i - 1]
 if sum1 > largest:
     largest = sum1
 if sum2 > largest:
