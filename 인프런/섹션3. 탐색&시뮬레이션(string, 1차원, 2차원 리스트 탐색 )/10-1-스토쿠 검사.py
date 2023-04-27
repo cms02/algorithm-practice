419dfd6305979d0074883718f232e65e80852901
cms02 <minoui1234@gmail.com>
2023-04-27 08:29:35 +0000

    섹션3연습

--- 인프런/섹션3. 탐색&시뮬레이션(string, 1차원, 2차원 리스트 탐색 )/10-1-스토쿠 검사.py
+++ 인프런/섹션3. 탐색&시뮬레이션(string, 1차원, 2차원 리스트 탐색 )/10-1-스토쿠 검사.py
@@ -1,29 +1,30 @@
 def check(a):
-  for i in range(9):
-    ch1 = [0] * 10
-    ch2 = [0] * 10
-    for j in range(9):
-      ch1[a[i][j]] = 1
-      ch2[a[i][j]] = 1
-    if sum(ch1) != 9 or sum(ch2) != 9:
-      return False
-  for i in range(3):
-    for j in range(3):
-      ch3 = [0] * 10
-      for k in range(3):
-        for s in range(3):
-          ch3[a[i*3+k][j*3+s]] = 1
-      if sum(ch3) != 9:
-        return False
-  return True
+    for i in range(9):
+        ch1 = [0] * 10
+        ch2 = [0] * 10
+        for j in range(9):
+            ch1[a[i][j]] = 1
+            ch2[a[j][i]] = 1
+        if sum(ch1) != 9 or sum(ch2) != 9:
+            return False
+    for i in range(3):
+        for j in range(3):
+            ch3 = [0] * 10
+            for k in range(3):
+                for s in range(3):
+                    ch3[a[i * 3 + k][j * 3 + s]] = 1
+            if sum(ch3) != 9:
+                return False
+    return True
+
 
 a = [list(map(int, input().split())) for _ in range(9)]
 if check(a):
-  print("YES")
+    print("YES")
 else:
-  print("NO")
+    print("NO")
 
-# 1 4 3 6 2 8 5 7 9 
+# 1 4 3 6 2 8 5 7 9
 # 5 7 2 1 3 9 4 6 8
 # 9 8 6 7 5 4 2 3 1
 # 3 9 1 5 4 2 7 8 6
