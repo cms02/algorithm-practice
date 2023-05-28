104ebcbba9abf2f15527274856760384c8ca4da5
cms02 <minoui1234@gmail.com>
2023-05-28 04:14:46 +0000

    section5-exercise

--- 인프런/excercise/5/3-후위표기식 만들기(스택).py
+++ 인프런/excercise/5/3-후위표기식 만들기(스택).py
@@ -7,18 +7,18 @@ for x in a:
     else:
         if x == '(':
             stack.append(x)
-        elif x == '*' or x == '/':
-            while stack and (stack[-1] == '*' or stack[-1] == '/'):
-                res += stack.pop()
-            stack.append(x)
-        elif x == '+' or x == '-':
-            while stack and stack[-1] != '(':
-                res += stack.pop()
-            stack.append(x)
         elif x == ')':
             while stack and stack[-1] != '(':
                 res += stack.pop()
             stack.pop()
+        elif x == '+' or x == '-':
+            while stack and stack[-1] != '(':
+                res += stack.pop()
+            stack.append(x)
+        elif x == '*' or x == '/':
+            while stack and (stack[-1] == '*' or stack[-1] == '/'):
+                res += stack.pop()
+            stack.append(x)
 while stack:
     res += stack.pop()
 print(res)
